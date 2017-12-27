e54c75cd91c54bb5b8cf5142735443b5f56fb67c
PolBarconsBou <polbarcons@gmail.com>
2017-12-27 17:27:41 +0100

    Provant FiltraActivity

---
+++ AdoptMe2/app/src/main/java/barcons/pol/adoptme/FiltraActivity.java
@@ -0,0 +1,13 @@
+package barcons.pol.adoptme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FiltraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtra);
+    }
+}

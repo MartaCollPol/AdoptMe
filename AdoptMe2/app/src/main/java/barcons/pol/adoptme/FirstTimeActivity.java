d8051ed2889961f0d6bdda3f9974fe83e7b7ad14
PolBarconsBou <polbarcons@gmail.com>
2018-01-12 18:39:26 +0100

    Habilitat botó a FirstTime per anar al Main

--- AdoptMe2/app/src/main/java/barcons/pol/adoptme/FirstTimeActivity.java
+++ AdoptMe2/app/src/main/java/barcons/pol/adoptme/FirstTimeActivity.java
@@ -1,13 +1,31 @@
 package barcons.pol.adoptme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
 
 public class FirstTimeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time);
+
+        Button next = (Button) findViewById(R.id.btn_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(view.getContext(), MainActivity.class);
+               startActivityForResult(intent,0);
+            }
+        });
+
+
+
     }
 }

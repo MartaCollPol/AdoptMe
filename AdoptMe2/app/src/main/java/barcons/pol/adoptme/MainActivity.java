103edab565a52d7e1fcb6d8e7f6d0230a7bd5a0f
Marta <maly-cat@hotmail.es>
2018-01-16 19:45:42 +0100

    Afegit traducció al català Afegits permisos de localització

--- AdoptMe2/app/src/main/java/barcons/pol/adoptme/MainActivity.java
+++ AdoptMe2/app/src/main/java/barcons/pol/adoptme/MainActivity.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
